fbac78f46cef7022c46854d5feaf4937e06db4c9
carlosJr-dev <jgomes14@outlook.com>
2024-04-09 22:24:40 -0300

    create structure

---
+++ src/main/java/br/com/api/billingnotification/BillingNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.billingnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/billingnotification/BillingNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.billingnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
